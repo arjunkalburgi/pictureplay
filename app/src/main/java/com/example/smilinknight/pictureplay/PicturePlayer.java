534a95a7050689b61e81e82bfb4f7c61d85573ec
Arjun Kalburgi <askalburgi@gmail.com>
2018-01-17 18:50:57 -0700

    create player activity

---
+++ app/src/main/java/com/example/smilinknight/pictureplay/PicturePlayer.java
@@ -0,0 +1,13 @@
+package com.example.smilinknight.pictureplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PicturePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+    }
+}
