a6fb3de0dc97536053af2e9fab1a54704114f71f
Arjun Kalburgi <askalburgi@gmail.com>
2018-01-20 01:18:53 -0700

    get image to show

--- app/src/main/java/com/example/smilinknight/pictureplay/PicturePlayer.java
+++ app/src/main/java/com/example/smilinknight/pictureplay/PicturePlayer.java
@@ -1,13 +1,24 @@
 package com.example.smilinknight.pictureplay;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class PicturePlayer extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+
+        Uri image_uri = getIntent().getData();
+
+        image = (ImageView) findViewById(R.id.imageView);
+        image.setImageURI(image_uri);
+
     }
 }
