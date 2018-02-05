581cf7e1c223a359c7aa7171f1ded316fd81c37c
Arjun Kalburgi <askalburgi@gmail.com>
2018-02-04 21:54:38 -0700

    completed swirl filter

--- app/src/main/java/com/example/smilinknight/pictureplay/filter/SwirlFilter.java
+++ app/src/main/java/com/example/smilinknight/pictureplay/filter/SwirlFilter.java
@@ -1,10 +1,6 @@
 package com.example.smilinknight.pictureplay.filter;
 
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.renderscript.Allocation;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptC;
 
 /**
  * Created by smilinknight on 2018-02-01.
@@ -15,55 +11,53 @@ public class SwirlFilter implements Filter {
     public Bitmap filter(Bitmap original, Bitmap image) {
 
         // This code originates from supercomputingblog.com
-        // credit goes to supercomputingblog.com
+        // with minor adaptions by myself to create the desired effect.
+        // Credit goes to supercomputingblog.com
         // http://supercomputingblog.com/openmp/image-twist-and-swirl-algorithm/
 
-        double factor = 0.00005;
-        double cX = original.getWidth()/2.0f;
-        double cY = original.getHeight()/2.0f;
+        double factor = 0.03;
+        double cW = (original.getWidth()-1)/2.0;
+        double cH = (original.getHeight()-1)/2.0;
 
-        int nPixels = original.getHeight()*original.getWidth();
-
-        for (int i=0; i < original.getHeight(); i++)
+        for (int h=0; h < original.getHeight(); h++)
         {
-            double relY = cY-i;
-            for (int j=0; j < original.getWidth(); j++)
+            double relH = cH-h;
+            for (int w=0; w < original.getWidth(); w++)
             {
-                double relX = j-cX;
-                // relX and relY are points in our UV space
+                double relW = w-cW;
+                // relW and relH are points in our UV space
                 // Calculate the angle our points are relative to UV origin. Everything is in radians.
                 double originalAngle;
-                if (relX != 0)
+                if (relW != 0)
                 {
-                    originalAngle = Math.atan(Math.abs(relY)/Math.abs(relX));
-                    if ( relX > 0 && relY < 0) originalAngle = 2.0f*Math.PI - originalAngle;
-                    else if (relX <= 0 && relY >=0) originalAngle = Math.PI-originalAngle;
-                    else if (relX <=0 && relY <0) originalAngle += Math.PI;
+                    originalAngle = Math.atan(Math.abs(relH)/Math.abs(relW));
+                    if (relW > 0 && relH < 0) originalAngle = 2.0*Math.PI - originalAngle;
+                    else if (relW <= 0 && relH >=0) originalAngle = Math.PI-originalAngle;
+                    else if (relW <=0 && relH <0) originalAngle += Math.PI;
                 }
                 else
                 {
                     // Take care of rare special case
-                    if (relY >= 0) originalAngle = 0.5f * Math.PI;
-                    else originalAngle = 1.5f * Math.PI;
+                    if (relH >= 0) originalAngle = 0.5 * Math.PI;
+                    else originalAngle = 1.5 * Math.PI;
                 }
                 // Calculate the distance from the center of the UV using pythagorean distance
-                double radius = Math.sqrt(relX*relX + relY*relY);
+                double radius = Math.sqrt(relW*relW + relH*relH);
                 // Use any equation we want to determine how much to rotate image by
-                //double newAngle = originalAngle + factor*radius; // a progressive twist
-                double newAngle = originalAngle + 1/(factor*radius+(4.0f/Math.PI));
+                double newAngle = originalAngle + factor*radius; // a progressive twist
                 // Transform source UV coordinates back into bitmap coordinates
-                int srcX = (int)(Math.floor(radius * Math.cos(newAngle)+0.5f));
-                int srcY = (int)(Math.floor(radius * Math.sin(newAngle)+0.5f));
-                srcX += cX;
-                srcY += cY;
-                srcY = original.getHeight() - srcY;
+                int srcW = (int) (Math.cos(newAngle) * radius);
+                int srcH = (int) (Math.sin(newAngle) * radius);
+                srcW += cW;
+                srcH += cH;
+                srcH = original.getHeight() - srcH;
                 // Clamp the source to legal image pixel
-                if (srcX < 0) srcX = 0;
-                else if (srcX >= original.getWidth()) srcX = original.getWidth()-1;
-                if (srcY < 0) srcY = 0;
-                else if (srcY >= original.getHeight()) srcY = original.getHeight()-1;
+                if (srcW < 0) srcW = 0;
+                else if (srcW >= original.getWidth()) srcW = original.getWidth()-1;
+                if (srcH < 0) srcH = 0;
+                else if (srcH >= original.getHeight()) srcH = original.getHeight()-1;
                 // Set the pixel color
-                image.setPixel(j, i, original.getPixel(srcX, srcY));
+                image.setPixel(w, h, original.getPixel(srcW, srcH));
             }
         }
 
